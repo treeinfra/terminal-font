a2f5aaa604351fd235474003a9d13d2885af9a9f
James Aprosail <aprosail@outlook.com>
2024-06-18 17:55:01 +0800

    color code and fix escape error

--- src/custom_color.rs
+++ src/custom_color.rs
@@ -1,9 +1,4 @@
-use crate::{
-    cancel,
-    custom::{background_rgb, foreground_rgb},
-    decorate::Decorate,
-    escape,
-};
+use crate::{cancel, custom::*, decorate::Decorate};
 
 pub struct RGBColor {
     pub r: u8,
@@ -11,19 +6,58 @@ pub struct RGBColor {
     pub b: u8,
 }
 
+impl RGBColor {
+    pub fn from(r: u8, g: u8, b: u8) -> RGBColor {
+        RGBColor { r, g, b }
+    }
+}
+
+/// Encapsulation on the [Decorate] trait with
+/// custom color escape codes for style decorations,
+/// including RGB mode ([RGBColor]) and color code mode.
+///
+/// Those methods won't check whether there are conflicts in the raw string.
+/// The only thing to do is wrap the prefix and the suffix.
+/// Those methods are usually used when content is predicated,
+/// and performance is more important.
+///
+/// ```rust
+/// use terminal_font::custom_color::*;
+///
+/// assert_eq!(
+///     " hello ".simple_rgb(RGBColor::from(143, 76, 78)),
+///     "\x1b[38;2;143;76;78m hello \x1b[39m",
+/// );
+///
+/// assert_eq!(
+///     " hello ".simple_bg_rgb(RGBColor::from(143, 76, 78)),
+///     "\x1b[48;2;143;76;78m hello \x1b[49m",
+/// );
+///
+/// assert_eq!(" hello ".simple_fg(123), "\x1b[38;5;123m hello \x1b[39m");
+/// assert_eq!(" hello ".simple_bg(123), "\x1b[48;5;123m hello \x1b[49m");
+/// ```
 pub trait SimpleCustomColor: Decorate {
     fn simple_rgb(&self, color: RGBColor) -> String {
         self.wrap(
             foreground_rgb(color.r, color.g, color.b),
-            escape!(cancel::FOREGROUND),
+            cancel::FOREGROUND,
         )
     }
 
     fn simple_bg_rgb(&self, color: RGBColor) -> String {
         self.wrap(
             background_rgb(color.r, color.g, color.b),
-            escape!(cancel::BACKGROUND),
+            cancel::BACKGROUND,
         )
+    }
+
+    fn simple_fg(&self, code: u8) -> String {
+        self.wrap(foreground_code(code), cancel::FOREGROUND)
+    }
+
+    fn simple_bg(&self, code: u8) -> String {
+        self.wrap(background_code(code), cancel::BACKGROUND)
     }
 }
 
